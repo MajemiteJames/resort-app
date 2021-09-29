b7107678a3e9babd2b594e749c5b43e91c068916
MajemiteJames <majemiteokoro@gmail.com>
2021-09-29 12:19:49 +0100

    first commit

---
+++ resort/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+import Error from './pages/Error'
+import Rooms from './pages/Rooms'
+import Home from './pages/Home'
+import SingleRoom from './pages/SingleRoom'
+
+import { Switch, Route } from "react-router-dom";
+
+import Navbar from './components/Navbar'
+
+
+function App() {
+  return <>
+    <Navbar />
+    <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+  </>
+}
+
+export default App;
