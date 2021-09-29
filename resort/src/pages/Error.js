366519e7394fb7d6f2cb70771a5c759aacf50f13
MajemiteJames <majemiteokoro@gmail.com>
2021-09-29 13:12:23 +0100

    created the service component

--- resort/src/pages/Error.js
+++ resort/src/pages/Error.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
-// import { Link } from "react-router-dom";
+ import { Link } from "react-router-dom";
 const Error = () => {
   return (
     <Hero>
       <Banner title="404" subtitle="page not found">
-        {/* <Link to="/" className="btn-primary">
+        <Link to="/" className="btn-primary">
           return home
-        </Link> */}
+        </Link>
       </Banner>
     </Hero>
   );
